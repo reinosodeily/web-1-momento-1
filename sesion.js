9859eab3984cb304c163a83273ecb71a1d4e46b8
reinosodeily <deilyyulia12@gmail.com>
2026-09-24 01:41:21 -0500

    se  pone punto y coma a las lineas de codigo

--- sesion.js
+++ sesion.js
@@ -5,22 +5,22 @@ let intentos= 3;
 
 function validarAcceso () { 
 
-const CONTRASENA_CORRECTA = "1234";
+  const CONTRASENA_CORRECTA = "1234";
 const USUARIO_CORRECTO = "admin";
  while (intentos > 0) {
     let usuario = prompt("ingrese el usuario");
-    let contrasena = prompt("ingrese el contrasena")
+    let contrasena = prompt("ingrese el contraseña");
 
     if(contrasena===CONTRASENA_CORRECTA && usuario===USUARIO_CORRECTO) {
-        alert("el usuario y contraseña son correctos")
+        alert("el usuario y contraseña son correctos");
         console.log("¡Bienvenido al sistema!");
         return ;
     } else {
         intentos--;
-        console.log("el usuario y contraseña es incorrecto")
-         alert(`le quedan ${intentos} intentos`)
+        console.log("el usuario y contraseña es incorrecto");
+         alert(`le quedan ${intentos} intentos`);
     }
 }
-    alert("Usuario bloqueado. Ha superado el número de intentos. ")
+    alert("Usuario bloqueado. Ha superado el número de intentos. ");
 } 
  
